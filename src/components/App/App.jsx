2c40f9cb4592591902aff6573b5aae5ea327098c
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-07-24 18:22:34 +0300

    feat: add Main Component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import styles from "./App.module.css";
 import Header from "../Header/Header";
+import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 
 const App = () => (
   <div className={styles.App}>
     <Header />
+    <Main />
     <Footer visible />
   </div>
 );
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,9 @@
+import styles from "./Main.module.css";
+
+const Main = () => (
+  <main className={styles.Main}>
+    <p>Main</p>
+  </main>
+);
+
+export default Main;
