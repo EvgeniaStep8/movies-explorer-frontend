491f8dfc28c2a2f72b1700e04bf986fe0c8cab5e
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-07-25 15:22:26 +0300

    feat: add techs component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import styles from "./Main.module.css";
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
 
 const Main = () => (
   <main className={styles.Main}>
     <Promo />
     <AboutProject />
+    <Techs />
   </main>
 );
 
