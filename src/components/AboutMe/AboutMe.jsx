42f86f34fa7ff8a461eea43701702102fb01a422
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-07-26 11:44:04 +0300

    feat: add AboutMe component

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,33 @@
+import styles from "./AboutMe.module.css";
+import SectionTitle from "../../UI/SectionTitle/SectionTitle";
+import avatar from "../../images/student.jpg";
+
+const AboutMe = () => (
+  <section className={styles.AboutMe}>
+    <SectionTitle title="Студент" />
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <h3 className={styles.title}>Евгения</h3>
+        <p className={styles.subtitle}>Фронтенд-разработчик, 26 лет</p>
+        <p className={styles.text}>
+          Разработка веб-приложений - это то, что меня по-настоящему увлекает и
+          приносит удовольствие. Я заинтересовалась веб-разработкой после
+          прохождения курсов повышения квалификации и создания небольшого
+          веб-приложения для студентов. Этот опыт показал мне, насколько
+          увлекательна и перспективна веб-разработка, и я решила изменить свою
+          жизнь в этом направлении.
+        </p>
+        <a
+          href="https://github.com/EvgeniaStep8"
+          target="blank"
+          className={styles.link}
+        >
+          Github
+        </a>
+      </div>
+      <img src={avatar} alt="аватар" className={styles.avatar} />
+    </div>
+  </section>
+);
+
+export default AboutMe;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,14 @@ import styles from "./Main.module.css";
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
+import AboutMe from "../AboutMe/AboutMe";
 
 const Main = () => (
   <main className={styles.Main}>
     <Promo />
     <AboutProject />
     <Techs />
+    <AboutMe />
   </main>
 );
 
