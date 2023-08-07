7dea7d73fc9cb4b47a781a23a685c2afe582ad12
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-08-07 17:41:45 +0300

    feat: add NotFoundPage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import styles from "./App.module.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Movies from "../Movies/Movies";
+import NotFoundPage from "../NotFoundPage/NotFoundPage";
 import Footer from "../Footer/Footer";
 
 const App = () => (
@@ -12,6 +13,7 @@ const App = () => (
       <Route path="/" element={<Main />} />
       <Route path="/movies" element={<Movies />} />
       <Route path="/saved-movies" element={<Movies savedMovies />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
     <Footer visible />
   </div>
