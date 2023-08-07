1c7926da3fc52597d9ce55e6c5bea9e804eb5954
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-08-07 15:28:36 +0300

    feat: add buuton(more) im Movies component

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -4,6 +4,11 @@ import MoviesCardList from "../MoviesCardList/MoviesCardList";
 const Movies = ({ savedMovies }) => (
   <div className={styles.Movies}>
     <MoviesCardList savedMovies={savedMovies} />
+    {!savedMovies && (
+      <button type="button" className={styles.button}>
+        Ещё
+      </button>
+    )}
   </div>
 );
 
