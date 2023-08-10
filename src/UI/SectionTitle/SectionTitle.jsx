6a3fc982881d2682d725e74a29414c8268ec6379
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-08-10 23:25:12 +0300

    reset

--- src/UI/SectionTitle/SectionTitle.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./SectionTitle.module.css";
-
-const SectionTitle = ({ title }) => (
-  <h2 className={styles.SectionTitle}>{title}</h2>
-);
-
-export default SectionTitle;
--- src/UI/TechsIcon/TechsIcon.jsx
+++
@@ -1,5 +0,0 @@
-import styles from "./TechsIcon.module.css";
-
-const TechsIcon = ({ text }) => <li className={styles.TechsIcon}>{text}</li>;
-
-export default TechsIcon;
